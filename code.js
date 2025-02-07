c98decae02413394561de92c1d2e9c08396d5f03
nataliesleight <nsleight@uwyo.edu>
2025-02-06 22:27:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,11 @@
 function sum(a) {
+    if (a.length == 0){
+        return 0;
+    }
+    else {
     var sum = a[0];
     for(var i = 1; i < a.length; i++) {
         sum += a[i];
     }
-    return sum;
+    return sum;}
 }
